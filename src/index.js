2474e888d2cabf49e7cea0e92c3d2c2a74e4de7d
piyush <piyusi045@gmail.com>
2025-08-07 20:30:45 +0530

    small debuging and routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"; /// beacuse it will connect through database
 import {DB_NAME} from "./constants.js";
 import connectDB from "./db/index.js";
+import {app} from './app.js'
 
 dotenv.config({
   path: './env'
